7fe053b538cf325b4fc25bb42fe8208f5fb38b8a
Kevin Siwicke <kevin.siwicke@noaa.gov>
2023-09-27 12:37:46 -0800

    back to sr for biom dl

--- code/biom_data_pull.R
+++ code/biom_data_pull.R
@@ -5,15 +5,15 @@ library(keyring)
 # I get survey data from AKFIN and my credentials are stored with keyring
 db <- "akfin"
 channel_akfin <- DBI::dbConnect (odbc::odbc(),
-                dsn = db,
-                uid = keyring::key_list(db)$username,
-                pwd =  keyring::key_get(db, keyring::key_list(db)$username))
+                                 dsn = db,
+                                 uid = keyring::key_list(db)$username,
+                                 pwd =  keyring::key_get(db, keyring::key_list(db)$username))
 
 # Get longline survey RPWs
 rpw <- dbGetQuery(channel_akfin, 
-                "select    *
+                  "select    *
                 from      afsc.lls_area_rpn_all_strata
-                where     species_code = '21230' and 
+                where     species_code = '30576' and 
                           fmp_management_area = 'GOA' and 
                           exploitable = 1 and 
                           country = 'United States'
@@ -38,16 +38,16 @@ cpue_dat <- left_join(data.frame('year' = rep(unique(cpue$year), each = 3), 'str
 
 # Get bottom trawl survey biomass data
 biom <- dbGetQuery(channel_akfin, 
-                "select    *
+                   "select    *
                 from      afsc.race_biomassstratumaigoa
-                where     species_code = '21230' and 
+                where     species_code = '30576' and 
                           survey = 'GOA' 
                 order by  year asc
                 ") %>% 
   rename_all(tolower)
 
 strata <- dbGetQuery(channel_akfin, 
-                    "select    *
+                     "select    *
                 from      afsc.race_goastrataaigoa
                 where     survey = 'GOA'
                 ") %>% 
@@ -57,21 +57,14 @@ biom2 <- left_join(biom, strata, by = c("stratum"))
 
 # Using all data like previous model
 biomass <- biom %>% 
-  mutate(strata = ifelse(stratum %in% c(10:13, 110:112, 210, 310), 'WGOA (0-500 m)',
-                         ifelse(stratum %in% c(20:35, 120:134, 220:232, 32, 320, 330), 'CGOA (0-500 m)',
-                                ifelse(stratum %in% c(40:50, 140:151, 240:251, 340:351), 'EGOA (0-500 m)',
-                                       ifelse(stratum == 410, 'WGOA (501-700 m)',
-                                              ifelse(stratum == 510, 'WGOA (701-1000 m)',
-                                                     ifelse(stratum %in% c(420, 430), 'CGOA (501-700 m)',
-                                                            ifelse(stratum %in% c(520, 530), 'CGOA (701-1000 m)',
-                                                                   ifelse(stratum %in% c(440, 450), 'EGOA (501-700 m)',
-                                                                          ifelse(stratum %in% c(540, 550), 'EGOA (701-1000 m)', NA)))))))))) %>% 
+  mutate(strata = ifelse(stratum %in% c(10:13, 110:112, 210, 310, 410, 510), 'WGOA',
+                         ifelse(stratum %in% c(20:35, 120:134, 220:232, 32, 320, 330, 420, 430, 520, 530), 'CGOA',
+                                ifelse(stratum %in% c(40:50, 140:151, 240:251, 340:351, 440, 450, 540, 550), 'EGOA', NA)))) %>% 
   group_by(year, strata) %>% 
-  summarize(n = n(), biomass = sum(stratum_biomass, na.rm = TRUE),
+  summarize(n = n(), total_catch = sum(catch_count), biomass = sum(stratum_biomass, na.rm = TRUE),
             cv = sqrt(sum(biomass_var, na.rm = TRUE))/biomass) 
 
-biomass_dat <- left_join(data.frame('year' = rep(unique(biomass$year), each = 9), 'strata' = rep(unique(biomass$strata), length(unique(biomass$year)))), biomass, by = c('year', 'strata')) %>% 
-  mutate(cv = ifelse (cv == 0, 0.1, cv)) 
+biomass_dat <- left_join(data.frame('year' = rep(unique(biomass$year), each = 3), 'strata' = rep(unique(biomass$strata), length(unique(biomass$year)))), biomass, by = c('year', 'strata'))
 
 model_yrs <- 1990:YEAR
 
